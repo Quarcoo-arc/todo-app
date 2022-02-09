269f90e996e556bc48ed8903ba714b01a677e5f8
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-02-09 11:21:26 +0000

    Clearing selected items

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const clearAllBtn = document.getElementById("clear");
 
 const form = document.getElementById("form");
 const taskList = document.getElementById("tasks");
+let id = 0;
+let selectedTasks = [];
 
 const showClearAllBtn = () => {
   clearAllBtn.classList.remove("hidden");
@@ -21,10 +23,28 @@ form.addEventListener("submit", (event) => {
 
   const newTaskItem = document.createElement("div");
 
-  newTaskItem.innerHTML = `<input type="checkbox" name="" id=""><li>${input}<li>`;
+  newTaskItem.innerHTML = `<input type="checkbox" name="" id="${id++}"><li>${input}<li>`;
+
   newTaskItem.addEventListener("click", (event) => {
-    if (event.target.tagName === "DIV") {
+    if (event.target.tagName === "DIV" || event.target.tagName === "LI") {
       event.target.classList.toggle("strikeThrough");
+    } else if (event.target.tagName === "INPUT") {
+      if (!selectedTasks.includes(event.target.closest("div"))) {
+        selectedTasks.push(event.target.closest("div"));
+        console.log(selectedTasks);
+
+        //enable clear button
+        clearAllBtn.disabled = false;
+      } else {
+        selectedTasks = selectedTasks.filter(
+          (el) => el !== event.target.closest("div")
+        );
+        if (selectedTasks.length === 0) {
+          clearAllBtn.disabled = true;
+        }
+        console.log(selectedTasks);
+      }
+      console.log(event.target);
     }
   });
   taskList.append(newTaskItem);
@@ -32,10 +52,15 @@ form.addEventListener("submit", (event) => {
 });
 
 clearAllBtn.addEventListener("click", (event) => {
+  //disable button
+  event.target.disabled = true;
+
   //Remove all children
-  const elements = [...taskList.children];
+  const elements = [...selectedTasks];
   elements.forEach((el) => el.remove());
 
   //Hide the button
-  event.target.classList.add("hidden");
+  if (taskList.children.length === 0) {
+    event.target.classList.add("hidden");
+  }
 });
